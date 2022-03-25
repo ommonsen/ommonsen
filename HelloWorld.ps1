bde3221172f064cf5fbd6ba8a6d8e78079a52d81
ommonsen <102366825+ommonsen@users.noreply.github.com>
2022-03-25 15:17:18 +0100

    Add files via upload

---
+++ HelloWorld.ps1
@@ -0,0 +1,9 @@
+#Write-Output 'Hello World!'
+
+#$name = Read-Host -Prompt "Please enter your name"
+#Write-Output "Congratulations $name! You have written your first code with PowerShell!"
+
+$date = Read-Host "Dagens dato"
+$name = Read-Host "Ditt navn"
+Write-Output " i dag er det $date."
+Write-Output "Jeg heter $name"
